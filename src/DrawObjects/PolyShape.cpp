60a6d00725d74940eae629a8dfbf6f98af941226
schlenkibus <justus.scheil@parteimail.de>
2018-09-01 12:22:25 +0200

    added terrain texture

--- src/DrawObjects/PolyShape.cpp
+++ src/DrawObjects/PolyShape.cpp
@@ -1,18 +1,21 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include "PolyShape.h"
+#include "../Tools/ResourceManager.h"
 
 PolyShape::PolyShape(const std::vector<sf::Vector2f> &polygons) {
     m_shape.setPointCount(polygons.size() + 2);
     std::size_t index = 1;
-    m_shape.setPoint(0, sf::Vector2f(0, 10000));
+    m_shape.setPoint(0, sf::Vector2f(0, 5000));
     for(auto& point: polygons) {
         m_shape.setPoint(index, point);
         index++;
     }
-    m_shape.setPoint(polygons.size() + 1, sf::Vector2f(m_shape.getPoint(index-1).x + 1, 10000));
-    m_shape.setOutlineColor(sf::Color::Green);
-    m_shape.setFillColor(sf::Color::Cyan);
+    m_shape.setPoint(polygons.size() + 1, sf::Vector2f(m_shape.getPoint(index-1).x + 1, 5000));
     m_shape.setOutlineThickness(1);
+    m_shape.setTexture(&ResourceManager::get().getTexture("/ground.png"));
+    ResourceManager::get().getTexture("/ground.png").setRepeated(true);
+    m_shape.setTextureRect(sf::IntRect(0, 0, static_cast<int>(m_shape.getLocalBounds().width),
+                                       static_cast<int>(m_shape.getLocalBounds().height)));
 }
 
 void PolyShape::draw(sf::RenderWindow &window) {
